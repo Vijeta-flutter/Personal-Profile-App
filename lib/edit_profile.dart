b09692da90b4f337f06c767cdf6c43d13a56727f
Vijeta-flutter <cvijeta2208@gmail.com>
2024-08-22 16:19:30 +0530

    initial commit

---
+++ lib/edit_profile.dart
@@ -0,0 +1,179 @@
+import 'dart:io';
+import 'package:first/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class EditProfile extends StatefulWidget {
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  File? _imagefile;
+  final ImagePicker _picker = ImagePicker();
+
+  var firstnameText = TextEditingController();
+  var lastnameText = TextEditingController();
+  var genderText = TextEditingController();
+  var bloodgroupText = TextEditingController();
+  var emailText = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getValues();
+  }
+
+  void getValues() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      firstnameText.text = prefs.getString('fname') ?? '';
+      lastnameText.text = prefs.getString('lname') ?? '';
+      genderText.text = prefs.getString('gender') ?? '';
+      bloodgroupText.text = prefs.getString('blood') ?? '';
+      emailText.text = prefs.getString('email') ?? '';
+      String? imagePath = prefs.getString('imagePath');
+      if (imagePath != null && imagePath.isNotEmpty) {
+        _imagefile = File(imagePath);
+      }
+    });
+  }
+
+  void pickImage(ImageSource source) async{
+    final XFile? image = await _picker.pickImage(source: source);
+    if (image != null){
+      setState(() {
+        _imagefile = File(image.path);
+      });
+    }
+  }
+
+  Future<void> setData() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('fname', firstnameText.text);
+    prefs.setString('lname', lastnameText.text);
+    prefs.setString('gender', genderText.text);
+    prefs.setString('blood', bloodgroupText.text);
+    prefs.setString('email', emailText.text);
+    if (_imagefile != null) {
+      await prefs.setString('imagePath', _imagefile!.path);
+    } else {
+      prefs.remove('imagePath');
+    }
+  }
+  void saveChanges() async{
+    await setData();
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile Page'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(
+                      height: 120,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.camera_alt),
+                            title: Text("Take a photo"),
+                            onTap: () {
+                              pickImage(ImageSource.camera);
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundImage: _imagefile != null
+                      ? FileImage(_imagefile!)
+                      :AssetImage('assets/new.png') as ImageProvider,
+                  child: _imagefile == null
+                      ? Icon(Icons.camera_alt, size: 50, color: Colors.black,)
+                      :null,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: firstnameText,
+                decoration: InputDecoration(
+                  hintText: "Your First Name",
+                  labelText: "First Name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: lastnameText,
+                decoration: InputDecoration(
+                  hintText: "Your Last Name",
+                  labelText: "Last Name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: genderText,
+                decoration: InputDecoration(
+                  hintText: "Your Gender",
+                  labelText: "Gender",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: bloodgroupText,
+                decoration: InputDecoration(
+                  hintText: "Your Blood Group",
+                  labelText: "Blood Group",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: emailText,
+                decoration: InputDecoration(
+                  hintText: "Your Email",
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed: saveChanges,
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 10,horizontal: 30
+                  ),
+                ),
+                child: Text(
+                  'Save Changes',
+                  style: TextStyle(
+                      fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'edit_profile.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _imagefile;
+
+  var firstname = "";
+  var lastname = "";
+  var gender = "";
+  var bloodgroup = "";
+  var email = "";
+
+  @override
+  void initState() {
+    getValues();
+    super.initState();
+  }
+
+  void getValues() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      firstname = prefs.getString('fname') ?? '';
+      lastname = prefs.getString('lname') ?? '';
+      gender = prefs.getString('gender') ?? '';
+      bloodgroup = prefs.getString('blood') ?? '';
+      email = prefs.getString('email') ?? '';
+      String? imagePath = prefs.getString('imagePath');
+      if (imagePath != null ) {
+        _imagefile = File(imagePath);
+      }
+    });
+  }
+ir
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile',
+          style: TextStyle(
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Center(
+            child: CircleAvatar(
+              radius: 70,
+              backgroundImage: _imagefile != null
+                  ? FileImage(_imagefile!)
+                  : AssetImage('assets/new.png') as ImageProvider,
+            ),
+          ),
+          SizedBox(height: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'First Name',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(firstname,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Last Name',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                lastname,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Gender',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                gender,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16,),
+              Text(
+                'Blood Group',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8,),
+              Text(
+                bloodgroup,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16,),
+              Text(
+                'Email address',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8,),
+              Text(
+                email,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder:(context) =>
+                        EditProfile()),).then((_){
+                          getValues();
+                  }
+                  );
+                },
+                child: Text('Edit Profile'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
